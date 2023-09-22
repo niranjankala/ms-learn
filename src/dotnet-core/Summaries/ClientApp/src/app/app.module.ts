813ad8849a7ce02b4cc23229e330f9c654f81023
Niranjan Singh <niranjan.kala@gmail.com>
2023-09-22 13:08:27 +0530

    Connecting the dots

--- src/dotnet-core/Summaries/ClientApp/src/app/app.module.ts
+++ src/dotnet-core/Summaries/ClientApp/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
-
 import { BooksComponent } from './components/books/books.component';
 import { DeleteBookComponent } from './components/delete-book/delete-book.component';
 import { NewBookComponent } from './components/new-book/new-book.component';
 import { ShowBookComponent } from './components/show-book/show-book.component';
 import { UpdateBookComponent } from './components/update-book/update-book.component';
 import { BookService } from './services/book.service';
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './store/book.reducer';
+import { createEffect, EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './store/book.effects';
 
 @NgModule({
   declarations: [
@@ -29,16 +32,18 @@ import { BookService } from './services/book.service';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'books', component: BooksComponent },
-      { path: 'new-book', component: NewBookComponent },
-      { path: 'update-book/:id', component: UpdateBookComponent },
-      { path: 'delete-book/:id', component: DeleteBookComponent },
-      { path: 'show-book/:id', component: ShowBookComponent }
-    ])
+      { path: 'books', component: BooksComponent},
+      { path: 'new-book', component: NewBookComponent},
+      { path: 'update-book/:id', component: UpdateBookComponent},
+      { path: 'delete-book/:id', component: DeleteBookComponent},
+      { path: 'show-book/:id', component: ShowBookComponent}
+    ]),
+    StoreModule.forRoot({applicationState: BookReducer}),
+    EffectsModule.forRoot([BookEffects])
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
