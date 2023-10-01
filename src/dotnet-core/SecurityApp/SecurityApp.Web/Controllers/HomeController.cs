a8a91620676652ad2d545db62d8be68e2b0ab637
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-01 18:04:28 +0530

    Simple authentication with [Authorize] attribute

--- src/dotnet-core/SecurityApp/SecurityApp.Web/Controllers/HomeController.cs
+++ src/dotnet-core/SecurityApp/SecurityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SecurityApp.Web.Models;
 using System.Diagnostics;
 
@@ -17,7 +18,7 @@ namespace SecurityApp.Web.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
