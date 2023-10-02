08ececf00ccd0ad647c2a1878eec7a2f329c01e4
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-02 20:47:02 +0530

    api-key auth

---
+++ src/dotnet-core/SecurityApp/SecurityApp.API/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecurityApp.API.Filters
+{
+    public class ApiKeyAuthAttribute
+    {
+        private const string CustomApiKeyParam = "X-API-Key";
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var apiKey = context.HttpContext.Request.Headers.TryGetValue(CustomApiKeyParam, out var apiKeyValue);
+
+            if (!apiKey)
+            {
+                context.Result = new UnauthorizedObjectResult("X-API-Key is missing in the header");
+            }
+
+            // apiKeyValue check
+            //if(apiKeyValue //does not exist)
+            //    context.Result = new UnauthorizedObjectResult("X-API-Key is not valid");
+
+            //if(apiKeyValue rate < //limit for hte apiKey)
+            //    context.Result = new UnauthorizedObjectResult($"{apiKeyValue} has exceeded the daily limit");
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
