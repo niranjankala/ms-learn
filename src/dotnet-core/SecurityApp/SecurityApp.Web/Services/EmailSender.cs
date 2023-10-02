c24c1346e018f94a8b78fd92e4af54235c6ec548
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-02 20:37:11 +0530

    Email account verification

---
+++ src/dotnet-core/SecurityApp/SecurityApp.Web/Services/EmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace SecurityApp.Web.Services
+{
+    public class EmailSender:IEmailSender
+    {
+    }
+}
