37878e57f272a87a3f2fb2a39cd30b41f070d984
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-08 16:19:54 +0530

    Add query to get single course

--- src/dotnet-core/Courses.GraphQL/Courses.GraphQL/GraphQL/Queries/CourseQuery.cs
+++ src/dotnet-core/Courses.GraphQL/Courses.GraphQL/GraphQL/Queries/CourseQuery.cs
@@ -1,5 +1,6 @@
 ﻿using Courses.GraphQL.Data.Repositories;
 using Courses.GraphQL.GraphQL.Types;
+using GraphQL;
 using GraphQL.Types;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,12 @@ namespace Courses.GraphQL.GraphQL.Queries
                 "courses",
                 "Returns list of courses",
                 resolve: context => repository.GetAllCourses());
+
+            Field<CourseType>(
+                "course",
+                "Returns a single course by id",
+                new QueryArguments(new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "id", Description = "Course Id" }),
+                resolve: context => repository.GetCourseById(context.GetArgument("id", int.MinValue)));
         }
     }
 }
