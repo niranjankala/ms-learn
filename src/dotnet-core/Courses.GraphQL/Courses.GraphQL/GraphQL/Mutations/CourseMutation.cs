6834009d8844d6d37e2b1c8ca1a3c053ac202404
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-10 11:33:32 +0530

    Adding your first mutation: Add data

---
+++ src/dotnet-core/Courses.GraphQL/Courses.GraphQL/GraphQL/Mutations/CourseMutation.cs
@@ -0,0 +1,30 @@
+﻿using Courses.GraphQL.Data.Models;
+using Courses.GraphQL.Data.Repositories;
+using Courses.GraphQL.GraphQL.Types;
+using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses.GraphQL.GraphQL.Mutations
+{
+    public class CourseMutation:ObjectGraphType
+    {
+        public CourseMutation(CoursesRepository repository)
+        {
+            Field<CourseType>(
+                "addCourse",
+                "Is used to add a new course to the database",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<CourseInputType>> { Name = "course", Description = "Course input parameter" }
+                    ),
+                resolve: context =>
+                {
+                    var course = context.GetArgument<Course>("course");
+                    return repository.AddCourse(course);
+                });
+        }
+    }
+}
---
+++ src/dotnet-core/Courses.GraphQL/Courses.GraphQL/GraphQL/Types/CourseInputType.cs
@@ -0,0 +1,22 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses.GraphQL.GraphQL.Types
+{
+    public class CourseInputType:InputObjectGraphType
+    {
+        public CourseInputType()
+        {
+            Name = "CourseInputType";
+
+            Field<StringGraphType>("Description");
+            Field<StringGraphType>("Name");
+            Field<IntGraphType>("Review");
+            Field<DateTimeGraphType>("DateAdded");
+            Field<DateTimeGraphType>("DateUpdated");
+        }
+    }
+}
