cad7d6f08962c529e4a1bd78ee6db8da904b571f
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-11 17:55:50 +0530

    Challenge: Deleting several items

--- src/dotnet-core/HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ src/dotnet-core/HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -95,5 +95,28 @@ namespace HPlusSport.API.Controllers
 
             return product;
         }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<ActionResult> DeleteMultiple([FromQuery]int[] ids)
+        {
+            var products = new List<Product>();
+            foreach (var id in ids)
+            {
+                var product = await _context.Products.FindAsync(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                products.Add(product);
+            }
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
     }
 }
