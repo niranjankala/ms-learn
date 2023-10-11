f742e299c20018ab6634c2a3dfdaa50865f56d48
Niranjan Singh <niranjan.kala@gmail.com>
2023-10-11 17:54:15 +0530

    Updating an item with PUT

--- src/dotnet-core/HPlusSport/HPlusSport.API/Program.cs
+++ src/dotnet-core/HPlusSport/HPlusSport.API/Program.cs
@@ -5,11 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
